5922823eea33fcc877eb985768b4508641ab9ce8
Bhanukiran Reddy <bkreddy889@gmail.com>
2025-08-31 12:18:15 +0530

    feat: setup SQLite database with users and scans tables and install morgan and import in index.js and active dev

--- oralvis-backend/index.js
+++ oralvis-backend/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
+const db = require("./db")
 require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"))
 
 const PORT = process.env.PORT || 5000;
 
